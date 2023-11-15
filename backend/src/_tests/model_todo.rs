5a90355145a2aea162bbfa098d0a6d33071193c7
Marcelo Santos <rocha.mer21@gmail.com>
2023-11-15 10:18:04 -0300

    model test get, create, update

--- backend/src/_tests/model_todo.rs
+++ backend/src/_tests/model_todo.rs
@@ -1,4 +1,10 @@
-use crate::model::{db::init_db, todo::{TodoPatch, TodoStatus}};
+use crate::{
+    model::{
+        db::init_db,
+        todo::{TodoPatch, TodoStatus},
+    },
+    security::utx_from_token,
+};
 
 use super::TodoMac;
 
@@ -7,18 +13,64 @@ async fn model_todo_create() -> Result<(), Box<dyn std::error::Error>> {
     // -- FIXTURE
     let db = init_db().await?;
 
+    let utx = utx_from_token("123").await?;
+
     let data_fx = TodoPatch {
         title: Some("text - model_todo_create 1".to_string()),
         ..Default::default()
     };
 
     // -- ACTION
-    let todo_created = TodoMac::create(&db, data_fx.clone()).await?;
+    let todo_created = TodoMac::create(&db, &utx, data_fx.clone()).await?;
 
     // -- CHECK
-    assert!(todo_created.id >= 1000, "Id should be >= 1000");
+    assert!(todo_created.id >= 100, "Id should be >= 100");
     assert_eq!(data_fx.title.unwrap(), todo_created.title);
     assert_eq!(TodoStatus::Open, todo_created.status);
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn model_todo_get() -> Result<(), Box<dyn std::error::Error>> {
+    let db = init_db().await?;
+
+    let utx = utx_from_token("123").await?;
+
+    let todo = TodoMac::get(&db, &utx, 100).await?;
+
+    assert_eq!(100, todo.id);
+    assert_eq!("todo 100", todo.title);
+    assert_eq!(TodoStatus::Close, todo.status);
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn model_todo_update() -> Result<(), Box<dyn std::error::Error>> {
+    let db = init_db().await?;
+
+    let utx = utx_from_token("123").await?;
+
+    let data_fx = TodoPatch {
+        title: Some("test - model_todo_update_ok 1".to_string()),
+        ..Default::default()
+    };
+
+    let todo_fx = TodoMac::create(&db, &utx, data_fx.clone()).await?;
+
+    let update_data_fx = TodoPatch {
+        title: Some("test - model_todo_update_ok 2".to_string()),
+        ..Default::default()
+    };
+
+    let todo = TodoMac::update(&db, &utx, todo_fx.id, update_data_fx.clone()).await?;
+
+    let todos = TodoMac::list(&db, &utx).await?;
+
+    assert_eq!(3, todos.len());
+    assert_eq!(todo_fx.id, todo.id);
+    assert_eq!(update_data_fx.title.unwrap(), todo.title);
 
     Ok(())
 }
@@ -28,8 +80,10 @@ async fn model_todo_list() -> Result<(), Box<dyn std::error::Error>> {
     // -- FIXTURE
     let db = init_db().await?;
 
+    let utx = utx_from_token("123").await?;
+
     // -- Action
-    let todos = TodoMac::list(&db).await?;
+    let todos = TodoMac::list(&db, &utx).await?;
 
     // -- CHECK
     assert_eq!(2, todos.len());
--- backend/src/model/todo.rs
+++ backend/src/model/todo.rs
@@ -2,7 +2,7 @@ use sqlb::HasFields;
 use warp::filters::query;
 
 use super::db::Db;
-use crate::model;
+use crate::{model, security::UserCtx};
 
 // region: Todo Types
 #[derive(sqlx::FromRow, Debug, Clone)]
@@ -35,25 +35,58 @@ sqlb::bindable!(TodoStatus);
 pub struct TodoMac;
 
 impl TodoMac {
-    pub async fn create(db: &Db, data: TodoPatch) -> Result<Todo, model::Error> {
+    const TABLE: &'static str = "todo";
+    const COLUMNS: &'static [&'static str] = &["id", "cid", "title", "status"];
+}
+
+impl TodoMac {
+    pub async fn create(db: &Db, utx: &UserCtx, data: TodoPatch) -> Result<Todo, model::Error> {
         let mut fields = data.not_none_fields();
         fields.push(("cid", 123).into());
 
         let sb = sqlb::insert()
-            .table("todo")
+            .table(Self::TABLE)
             .data(fields)
-            .returning(&["id", "cid", "title", "status"]);
+            .returning(Self::COLUMNS);
 
         let todo = sb.fetch_one(db).await?;
 
         Ok(todo)
     }
 
-    pub async fn list(db: &Db) -> Result<Vec<Todo>, model::Error> {
-       let sb = sqlb::select()
-       .table("todo")
-       .columns(&["id", "cid", "title", "status"])
-       .order_by("!id");
+    pub async fn get(db: &Db, _utx: &UserCtx, id: i64) -> Result<Todo, model::Error> {
+        let sb = sqlb::select()
+            .table(Self::TABLE)
+            .columns(Self::COLUMNS)
+            .and_where_eq("id", id);
+
+        let todo = sb.fetch_one(db).await?;
+
+        Ok(todo)
+    }
+
+    pub async fn update(
+        db: &Db,
+        _utx: &UserCtx,
+        id: i64,
+        data: TodoPatch,
+    ) -> Result<Todo, model::Error> {
+        let sb = sqlb::update()
+            .table(Self::TABLE)
+            .data(data.not_none_fields())
+            .and_where_eq("id", id)
+            .returning(Self::COLUMNS);
+
+        let todo = sb.fetch_one(db).await?;
+
+        Ok(todo)
+    }
+
+    pub async fn list(db: &Db, _utx: &UserCtx) -> Result<Vec<Todo>, model::Error> {
+        let sb = sqlb::select()
+            .table(Self::TABLE)
+            .columns(Self::COLUMNS)
+            .order_by("!id");
 
         let todos = sb.fetch_all(db).await?;
 
